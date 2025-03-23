d77cd28b344587e82940fd4bbba61aa0b9fd7ba1
MQuyen123 <minhquyenho09@gmail.com>
2025-03-23 09:15:03 +0700

    Setting up screen navigation and handling button press events

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sound_nest/ui/home/view_model.dart';
 
 import '../../data/model/song.dart';
+import '../now_playing/playing.dart';
 
 class MusicApp extends StatelessWidget {
   const MusicApp({super.key});
@@ -30,81 +32,110 @@ class HomeTabPage extends StatefulWidget {
 
 class _HomeTabPageState extends State<HomeTabPage> {
   List<Song> songs = [];
-  late  MusicAppViewModel _viewModel =  MusicAppViewModel();
+  late MusicAppViewModel _viewModel = MusicAppViewModel();
 
-  initState(){
+  @override
+  initState() {
     _viewModel = MusicAppViewModel();
     _viewModel.loadSongs();
     observeData();
     super.initState();
-
-
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: getBody(),
-    );
+    return Scaffold(body: getBody());
   }
 
   @override
-  dispose(){
+  dispose() {
     super.dispose();
     _viewModel.songStream.close();
   }
 
-  Widget getBody(){
+  Widget getBody() {
     bool showLoading = songs.isEmpty;
-    if(showLoading){
+    if (showLoading) {
       return getProgressBar();
-    }
-    else {
+    } else {
       return getListView();
     }
   }
 
-  Widget getProgressBar(){
-    return const Center(
-      child: CircularProgressIndicator(),
-    );
+  Widget getProgressBar() {
+    return const Center(child: CircularProgressIndicator());
   }
 
-  ListView getListView(){
+  ListView getListView() {
     return ListView.separated(
-        itemCount: songs.length,
-        shrinkWrap: true,
-        separatorBuilder: (context, index) => const Divider(
-          color: Colors.grey,
-          thickness: 1,
-          indent: 20,
-          endIndent: 20,
-        ),
-        itemBuilder: (context, position) {
-          return getRow(position);
-        }
+      itemCount: songs.length,
+      shrinkWrap: true,
+      separatorBuilder:
+          (context, index) => const Divider(
+            color: Colors.grey,
+            thickness: 1,
+            indent: 20,
+            endIndent: 20,
+          ),
+      itemBuilder: (context, position) {
+        return getRow(position);
+      },
     );
   }
 
-  Widget getRow(int index){
-    return _SongItemSection(parent: this , song: songs[index]);
+  Widget getRow(int index) {
+    return _SongItemSection(parent: this, song: songs[index]);
   }
 
-  void observeData(){
+  void observeData() {
     _viewModel.songStream.stream.listen((songList) {
       setState(() {
         songs.addAll(songList);
       });
     });
   }
+
+  void showBottomSheet(Song song) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+          child: Container(
+            color: Colors.grey.shade50,
+            height: double.maxFinite,
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text("Show bottom sheet for ${song.title}"),
+                ElevatedButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: Text("Close Button Sheet")),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void navigateToSongDetail(Song song) {
+    Navigator.push(
+      context,
+      CupertinoPageRoute(
+        builder: (context) => NowPlaying(songs: songs, playingSong: song),
+      ),
+    );
+  }
 }
 
-
 class _SongItemSection extends StatelessWidget {
-   const _SongItemSection({
-     required this.parent,
-     required this.song,
-   });
+  const _SongItemSection({required this.parent, required this.song});
 
   final _HomeTabPageState parent;
   final Song song;
@@ -115,26 +146,34 @@ class _SongItemSection extends StatelessWidget {
       contentPadding: const EdgeInsets.only(left: 20, right: 20),
       leading: ClipRRect(
         borderRadius: BorderRadius.circular(10),
-        child: FadeInImage.assetNetwork(placeholder: 'assets/images/loading.jpg',
-            image: song.image,
-            width: 50,
-            height: 50,
-            imageErrorBuilder: (context, error, stackTrace) {
-              return Image.asset('assets/images/loading.jpg',
-                width: 50,
-                height: 50,
-
-              );
-            }
+        child: FadeInImage.assetNetwork(
+          placeholder: 'assets/images/loading.jpg',
+          image: song.image,
+          width: 50,
+          height: 50,
+          imageErrorBuilder: (context, error, stackTrace) {
+            return Image.asset(
+              'assets/images/loading.jpg',
+              width: 50,
+              height: 50,
+            );
+          },
         ),
       ),
-      title: Text(song.title, style: const TextStyle(fontWeight: FontWeight.bold)),
+      title: Text(
+        song.title,
+        style: const TextStyle(fontWeight: FontWeight.bold),
+      ),
       subtitle: Text(song.artist),
       trailing: IconButton(
-          icon: const Icon(Icons.more_horiz),
-          onPressed: (){},
-      )
+        icon: const Icon(Icons.more_horiz),
+        onPressed: () {
+          parent.showBottomSheet(song);
+        },
+      ),
+      onTap: () {
+        parent.navigateToSongDetail(song);
+      },
     );
   }
-
 }
