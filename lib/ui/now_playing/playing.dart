76e9de41017f4470fa346704e8fb2bd0d5760493
MQuyen123 <minhquyenho09@gmail.com>
2025-03-23 15:25:46 +0700

    Design the music player interface

--- lib/ui/now_playing/playing.dart
+++ lib/ui/now_playing/playing.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../../data/model/song.dart';
 
@@ -8,15 +9,16 @@ class NowPlaying extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return NowPlayingPage(
-        songs: songs,
-      playingSong: playingSong,
-    );
+    return NowPlayingPage(songs: songs, playingSong: playingSong);
   }
 }
 
 class NowPlayingPage extends StatefulWidget {
-  const NowPlayingPage({super.key, required this.songs, required this.playingSong});
+  const NowPlayingPage({
+    super.key,
+    required this.songs,
+    required this.playingSong,
+  });
 
   final Song playingSong;
   final List<Song> songs;
@@ -28,11 +30,33 @@ class NowPlayingPage extends StatefulWidget {
 class _NowPlayingPageState extends State<NowPlayingPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Now Playing'),
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: const Text('Now Playing'),
+        trailing: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.more_horiz),
+        ),
+      ),
+      child: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: FadeInImage.assetNetwork(
+                  placeholder: 'assets/images/loading.jpg',
+                  image: widget.playingSong.image,
+                ),
+              ),
+            ),
+            Text(widget.playingSong.title),
+            Text(widget.playingSong.artist),
+          ],
+        ),
       ),
     );
   }
 }
-
